312fb3fa2b3f8e21d6f9c2d3c3c359d321c7af3d
TanvirSajib <tanvir.sajib.programmer@gmail.com>
2025-01-21 09:41:03 +0600

    (d/a) 31. Adding an Option to Change the Background and Text Colours

--- src/create-simple-block-udemy-pactice/save.js
+++ src/create-simple-block-udemy-pactice/save.js
@@ -1,12 +1,18 @@
 import { useBlockProps, RichText } from '@wordpress/block-editor';
 
 export default function save( { attributes } ) {
-	const { text, alignment } = attributes;
+
+	const { text, alignment, backgroundColor, textColor } = attributes;
+
 	return (
 		<RichText.Content
-			{ ...useBlockProps.save({
+			{ ...useBlockProps.save( {
 				className: `text-box-align-${ alignment }`,
-			}) }
+				style: {
+					backgroundColor,
+					color: textColor,
+				},
+			} ) }
 			tagName="h4"
 			value={ text }
 		/>
