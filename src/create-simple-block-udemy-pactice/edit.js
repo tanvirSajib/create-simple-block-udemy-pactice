39b0a21f98db8237bef39ed79973190a6b99cc7c
TanvirSajib <tanvir.sajib.programmer@gmail.com>
2025-02-04 15:05:34 +0600

    35. Adding an Option for toggling Box Shadow

--- src/create-simple-block-udemy-pactice/edit.js
+++ src/create-simple-block-udemy-pactice/edit.js
@@ -7,7 +7,9 @@ import {
 	InspectorControls,
 } from '@wordpress/block-editor';
 import './editor.scss';
-import { BoxControl } from '@wordpress/components';
+import { PanelBody,
+	RangeControl, } from '@wordpress/components';
+import classnames from 'classnames';
 
 
 
@@ -16,7 +18,7 @@ import { BoxControl } from '@wordpress/components';
 
 export default function Edit( props ) {
 	const { attributes, setAttributes } = props;
-	const { text, alignment } = attributes;
+	const { text, alignment, style, shadow, shadowOpacity } = attributes;
 
 	const onChangeAlignment = ( newAlignment ) => {
 		setAttributes( { alignment: newAlignment } );
@@ -24,23 +26,58 @@ export default function Edit( props ) {
 	const onChangeText = ( newText ) => {
 		setAttributes( { text: newText } );
 	};
+	const onChangeShadowOpacity = ( newShadowOpacity ) => {
+		setAttributes( { shadowOpacity: newShadowOpacity } );
+	};
+
+	const toggleShadow = () => {
+		setAttributes( { shadow: ! shadow } );
+	};
+	const classes = classnames( `text-box-align-${ alignment }`, {
+		'has-shadow': shadow,
+		[ `shadow-opacity-${ shadowOpacity }` ]: shadow && shadowOpacity,
+	} );
+
 
 	
 
 	return (
 		<>
-		<div { ...useBlockProps( {
-					className: `text-box-align-${ alignment }`,
-				} ) }
-				>
-			
 		
-			<BlockControls>
+			
+			<InspectorControls>
+			{ shadow && (
+					<PanelBody title={ __( 'Shadow Setting', 'text-box' ) }>
+						<RangeControl
+							label={ __( 'Shadow Opacity', 'text-box' ) }
+							value={ shadowOpacity }
+							min={ 10 }
+							max={ 40 }
+							step={ 10 }
+							onChange={ onChangeShadowOpacity }
+						/>
+					</PanelBody>
+				) }
+			</InspectorControls>
+		
+			<BlockControls
+			controls={ [
+				{
+					icon: 'admin-page',
+					title: __( 'Shadow', 'text-box' ),
+					onClick: toggleShadow,
+					isActive: shadow,
+				},
+			] }>
 				<AlignmentToolbar
 					value={ alignment }
 					onChange={ onChangeAlignment }
 				/>
 			</BlockControls>
+			<div { ...useBlockProps( {
+					className: classes,
+				} ) }
+				>
 			<RichText
 				className="text-box-title"				
 				onChange={ onChangeText }
